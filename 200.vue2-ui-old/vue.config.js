9d913566460569bd339e9db270eb1eafc46311b3
langnang <langnang.chen@outlook.com>
2024-04-26 16:01:10 +0800

    vue2-ui

--- 200.vue2-ui-old/vue.config.js
+++ 200.vue2-ui-old/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 基本路径
-  publicPath: process.env.NODE_ENV === "production" ? "/200.vue2-ui/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue2-ui/" : "/",
   // 构建时的输入目录
   outputDir: "dist",
   // 配置 webpack-dev-server 行为
@@ -10,10 +10,10 @@ module.exports = {
   },
   // 接收一个基于 webpack-chain 的 ChainableConfig 实例。
   // 允许对内部的 webpack 配置进行更细粒度的修改。
-  // chainWebpack: function (config) {
-  //   config.plugin("html").tap(function (args) {
-  //     args[0].title = "Vue - Langnang Template"; // 自定义标题
-  //     return args;
-  //   });
-  // }
+  chainWebpack: function (config) {
+    config.plugin("html").tap(function (args) {
+      args[0].title = "Vue - Langnang Template"; // 自定义标题
+      return args;
+    });
+  }
 };
